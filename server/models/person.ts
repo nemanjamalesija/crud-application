e4c58d4876a64ca96c1b98200b3870394ba4395c
nemanjamalesija <malesija13@gmail.com>
2023-04-05 01:18:16 +0200

    Create Person model

---
+++ server/models/person.ts
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose';
+
+const personSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: [true, 'A person must have a name'],
+  },
+  lastName: {
+    type: String,
+    required: [true, 'A person must have a last name'],
+  },
+  age: {
+    type: Number,
+    requred: [true, 'The age must be defined'],
+  },
+  city: String,
+  adress: String,
+  createdDate: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+export const Person = mongoose.model('Person', personSchema);
