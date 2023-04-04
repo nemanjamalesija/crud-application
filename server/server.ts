6f689a1b3cba7b18ae2c603d4ff07803d28a949c
nemanjamalesija <malesija13@gmail.com>
2023-04-04 18:03:10 +0200

    Test the database

--- server/server.ts
+++ server/server.ts
@@ -24,6 +24,46 @@ mongoose
     console.log(error);
   });
 
+const personSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: [true, 'A person must have a name'],
+  },
+  lastName: {
+    type: String,
+    required: [true, 'A person must have a last name'],
+  },
+  age: {
+    type: Number,
+    requred: [true, 'The age must be defined'],
+  },
+  city: String,
+  adress: String,
+  createdDate: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+const Person = mongoose.model('Person', personSchema);
+
+const testPerson = new Person({
+  firstName: 'Drazen',
+  lastName: 'Stosic',
+  age: 25,
+});
+
+const savePerson = async () => {
+  try {
+    const newPerson = await testPerson.save();
+    console.log(newPerson);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+savePerson();
+
 server.listen(process.env.PORT, () => {
   console.log(`Server is listening on port ${process.env.PORT}`);
 });
