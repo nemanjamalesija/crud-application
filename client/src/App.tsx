900e813c825119cd4b5abc37839ff131837bdf4e
nemanjamalesija <malesija13@gmail.com>
2023-04-05 22:33:53 +0200

    Install axios. Fetch people from the client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,22 @@
+import axios from 'axios';
+import { useEffect } from 'react';
+
 function App() {
+  useEffect(() => {
+    const getAllUsers = async () => {
+      const response = await axios('http://127.0.0.1:3000/api/people/');
+      const {
+        data: {
+          data: { people },
+        },
+      } = response;
+
+      console.log(people);
+    };
+
+    getAllUsers();
+  }, []);
+
   return <div className='App'></div>;
 }
 
