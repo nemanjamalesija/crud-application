468a4a9a09e40ef7cd4d1388bf8a79db3002f767
nemanjamalesija <malesija13@gmail.com>
2023-04-11 00:15:54 +0200

    Optimize performance

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,33 +1,15 @@
 import Categories from './components/Categories';
-import { useAppDispatch } from './hooks/useAppDispatch';
-import { useAppSelector } from './hooks/useAppSelector';
-import { toggleForm } from './reducers/globalStateReducer';
-import { useGetPeopleData } from './hooks/useGetPeople';
 import Form from './components/Form';
 import Header from './components/Header';
 import People from './components/People';
 
 function App() {
-  const { showMainForm } = useAppSelector((state) => state.globalStateReducer);
-  const { loading, error } = useGetPeopleData();
-  const dispatch = useAppDispatch();
-
-  if (loading) return <h1>Loading...</h1>;
-
-  if (error) return <h1>`Sorry, there has been an error: ${error}`</h1>;
-
-
-
   return (
     <div className='app'>
       <Form />
       <Header />
       <Categories />
       <People />
-      <div
-        className={`${showMainForm ? 'overlay' : 'overlay hidden'}`}
-        onClick={() => dispatch(toggleForm())}
-      ></div>
     </div>
   );
 }
--- client/src/components/People.tsx
+++ client/src/components/People.tsx
@@ -1,11 +1,19 @@
+import { useAppDispatch } from '../hooks/useAppDispatch';
+import { useAppSelector } from '../hooks/useAppSelector';
 import { useGetPeopleData } from '../hooks/useGetPeople';
+import { toggleForm } from '../reducers/globalStateReducer';
 import EditForm from './EditForm';
 import Person from './Person';
 
 const People = () => {
   const { peopleSTATE, currentPersonID } = useGetPeopleData();
+  const { showMainForm } = useAppSelector((state) => state.globalStateReducer);
+  const { loading, error } = useGetPeopleData();
+  const dispatch = useAppDispatch();
 
-  console.log(peopleSTATE);
+  if (loading) return <h1>Loading...</h1>;
+
+  if (error) return <h1>`Sorry, there has been an error: ${error}`</h1>;
 
   return (
     <div className='people-container'>
@@ -16,6 +24,10 @@ const People = () => {
           <Person key={person._id} {...person} />
         );
       })}
+      <div
+        className={`${showMainForm ? 'overlay' : 'overlay hidden'}`}
+        onClick={() => dispatch(toggleForm())}
+      ></div>
     </div>
   );
 };
